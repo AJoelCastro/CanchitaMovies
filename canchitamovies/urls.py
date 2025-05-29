fb88e4f4da59d4fb965ab7ba62c6188ce916600c
AJoelCastro <misthorn33@gmail.com>
2025-05-29 15:32:39 -0500

    routes createds

--- canchitamovies/urls.py
+++ canchitamovies/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.menu, name='menu'),
+    path('generos/', include('generos.urls'), name='generos'),
+    path('movies/', include('movies.urls'), name='movies'),
+    path('users/', include('users.urls'), name='usuarios'),
+    path('series/', include('series.urls'), name='series'),
 ]
