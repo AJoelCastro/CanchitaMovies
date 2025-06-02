250dd602fa7e23593df777f4d82a0afca032fcc4
Elluisito30 <dejerbakers1@gmail.com>
2025-06-02 12:23:33 -0500

    cambios1

--- movies/views.py
+++ movies/views.py
@@ -14,12 +14,6 @@ class MovieDetailView(DetailView):
     template_name = 'movies/movie_detail.html'
     context_object_name = 'movie'
 
-class SeriesListView(ListView):
-    model = Series
-    template_name = 'movies/series_list.html'
-    context_object_name = 'series'
-    paginate_by = 12
-
 # Create your views here.
 def menu(request):
     return render(request,'movies/menu.html')
